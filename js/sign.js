de4aa82421d6245fb2b11cb9cf94603dcaaf4d38
drengashi7 <167891457+drengashi7@users.noreply.github.com>
2024-12-06 20:33:20 +0100

    final sign.html

--- js/sign.js
+++ js/sign.js
@@ -1,10 +1,25 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    const menuBtn = document.getElementById('menu-btn');
-    const navLinks = document.getElementById('nav-links');
 
-    menuBtn.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-        menuBtn.classList.toggle('active');
-    });
+
+
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = document.getElementById("i");
+
+const dots = document.querySelectorAll('.dot');
+
+menuBtn.addEventListener("click", function() {
+    navLinks.classList.toggle("open")
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen?"ri-close-line" : "ri-menu-list")
 });
+
+navLinks.addEventListener("click", function(){
+    navLinks.classList.remove("open")
+    menuBtnIcon.setAttribute("class", "ri-menu-line")
+})
+document.querySelector('.view-all').addEventListener('click', () => {
+    alert('View all destinations clicked!');
+});
+
