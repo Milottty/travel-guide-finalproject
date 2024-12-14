480b02fd710b9590ad8993d264c5f9cc6b1c5976
drengashi7 <167891457+drengashi7@users.noreply.github.com>
2024-12-14 12:51:50 +0100

    sign in is finished GG

--- js/sign.js
+++ js/sign.js
@@ -19,7 +19,71 @@ navLinks.addEventListener("click", function(){
     navLinks.classList.remove("open")
     menuBtnIcon.setAttribute("class", "ri-menu-line")
 })
-document.querySelector('.view-all').addEventListener('click', () => {
-    alert('View all destinations clicked!');
-});
+// document.querySelector('.view-all').addEventListener('click', () => {
+//     alert('View all destinations clicked!');
+// });
 
+
+
+function checkPassword() {
+    var password = document.getElementById("password").value;
+    var numberRequirement = document.getElementById("numberRequirement");
+    var symbolRequirement = document.getElementById("symbolRequirement");
+    var passwordInput = document.getElementById("password");
+
+  
+    if (/\d/.test(password)) {
+        numberRequirement.classList.remove("invalid");
+        numberRequirement.classList.add("valid");
+    } else {
+        numberRequirement.classList.remove("valid");
+        numberRequirement.classList.add("invalid");
+    }
+
+   
+    if (/[@#%^:$;&/]/.test(password)) {
+        symbolRequirement.classList.remove("valid");
+        symbolRequirement.classList.add("invalid");
+        passwordInput.classList.add("invalid-input"); 
+    } else {
+        symbolRequirement.classList.remove("invalid");
+        symbolRequirement.classList.add("valid");
+        passwordInput.classList.remove("invalid-input"); 
+    }
+}
+
+function validatePassword() {
+    var password = document.getElementById("password").value;
+    if (/[@#%^:$;&/]/.test(password)) {
+        alert("Password contains invalid symbols!");
+        return false;
+    }
+    return true;
+}
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const togglePasswordButton = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('password');
+
+    // Add event listener to toggle password visibility
+    togglePasswordButton.addEventListener('click', function () {
+        // Check the current input type
+        const inputType = passwordInput.type;
+
+        // Toggle the type between 'password' and 'text'
+        if (inputType === 'password') {
+            passwordInput.type = 'text';
+            togglePasswordButton.classList.remove('fa-eye');
+            togglePasswordButton.classList.add('fa-eye-slash');  // Change icon to 'eye-slash'
+        } else {
+            passwordInput.type = 'password';
+            togglePasswordButton.classList.remove('fa-eye-slash');
+            togglePasswordButton.classList.add('fa-eye');  // Change icon to 'eye'
+        }
+    });
+});
